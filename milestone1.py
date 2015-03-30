9175c31fbe6baacb80ab0efee2193ca5b9f729db
Justin Chiu <justc@seas.upenn.edu>
2015-03-30 18:53:13 -0400

    started script

--- milestone1.py
+++ milestone1.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python
 
 from mininet.net import Mininet
+#from mininet.node import Controller
 from mininet.topo import Topo
+
+def mutate_net(net):
+    AA_h1 = net.addHost('AA_h1')
+    AA_h2 = net.addHost('AA_h2')
+    AB_h1 = net.addHost('AB_h1')
+    AB_h2 = net.addHost('AB_h2')
+    BA_h1 = net.addHost('BA_h1')
+    BA_h2 = net.addHost('BA_h2')
+    BB_h1 = net.addHost('BB_h1')
+    BB_h2 = net.addHost('BB_h2')       
+
+    ca = net.addController('ca')
+    cb = net.addController('cb')
+
+    A = net.addSwitch('A')
+    AA = net.addSwitch("AA")
+    AB = net.addSwitch("AB")
+
+    B = net.addSwitch('B')
+    BA = net.addSwitch("BA")
+    BB = net.addSwitch("BB")
+            
+    net.addLink(h1,s1)
+    net.addLink(h2,s1)
+        
+    return net
+
+def run():
+    net = mutate_net(Mininet(topo))
+    
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Testing network connectivity"
+    net.pingAll()
+    net.stop()
+
+        
+if __name__ == "__main__":
+    run()
